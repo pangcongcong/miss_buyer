3780aab22a283e0b5569a0e5d8d97cd15e242919
庞敬聪 <1043573731@qq.com>
2019-12-19 17:05:58 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ECharts from 'vue-echarts'
+import 'echarts-gl'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+Vue.component('v-chart', ECharts)
